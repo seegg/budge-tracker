cd95937f083354e0f2dede919f90700856264aa8
yuan tan <dyxtan@gmail.com>
2022-04-14 10:52:44 +1200

    add auth route handlers

---
+++ src/auth/auth-controller.ts
@@ -0,0 +1,37 @@
+import { RequestHandler } from "express";
+import authServiceModule from "./auth-services";
+
+export const authController = (authServices = authServiceModule) => {
+
+  const postLogin: RequestHandler = async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      const accessToken = authServices.logInByEmail(email, password);
+      res.json({ accessToken });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  const postRegister: RequestHandler = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  const postVerify: RequestHandler = async (req, res, next) => { };
+
+  const postRefreshToken: RequestHandler = async (req, res, next) => { };
+
+  return {
+    postLogin,
+    postRegister,
+    postVerify,
+    postRefreshToken,
+  };
+
+};
+
+export default authController();
